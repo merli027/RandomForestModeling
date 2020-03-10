664409931f010a5cd84371f045daeebdd03c3946
merli027 <merli027@umn.edu>
2020-03-10 13:57:13 +0000

    Getting furhter

--- server.py
+++ server.py
@@ -4,7 +4,7 @@ from jinja2 import StrictUndefined
 
 from flask import Flask, render_template, request, flash, redirect, session
 from flask_debugtoolbar import DebugToolbarExtension
-from model import connect_to_db, db, Talk, Speaker
+from model import connect_to_db, db, Talk, Speaker, Rating, Talk_Rating
 
 
 app = Flask(__name__)
@@ -28,6 +28,7 @@ def talk_list():
     """Show list of talks."""
 
     talks = Talk.query.all()
+    
     return render_template("talk_list.html", talks=talks)
 
 @app.route("/speakers")
@@ -36,6 +37,14 @@ def speaker_list():
 
     speakers = Speaker.query.all()
     return render_template("speaker_list.html", speakers=speakers)
+
+@app.route("/talks/<int:talk_id>", methods=['GET'])
+def talk_detail(talk_id):
+    """Show information of ."""
+
+    talk=Talk.query.get(talk_id)
+    talk_rating=Talk_Rating.query.filter_by(ted_talk_id=talk_id).all()
+    return render_template("talk.html", talk=talk, talk_rating=talk_rating)
 
 if __name__ == "__main__":
     # We have to set debug=True here, since it has to be True at the point
